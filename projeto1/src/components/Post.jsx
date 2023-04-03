55d3086fa2ddff6eed8e2ba2f6d7077f72d51d6a
Víctor Barreto <victor.barreto@behoh.com>
2023-04-03 17:43:31 -0300

    propriedades do Post

--- projeto1/src/components/Post.jsx
+++ projeto1/src/components/Post.jsx
@@ -1,56 +1,72 @@
+import { format, formatDistanceToNow } from "date-fns";
+import { ptBR } from "date-fns/locale";
 import { Avatar } from "./Avatar";
 import { Comment } from "./Comment";
 
 import styles from "./Post.module.css";
 
-export function Post() {
-	return (
-		<article className={styles.post}>
-			<header>
-				<div className={styles.author}>
-					<Avatar src="https://avatars.githubusercontent.com/u/88116771?v=4" />
+export function Post({ author, publishedAt, content }) {
+    const publishedDateFormatted = format(
+        publishedAt,
+        "d 'de' LLLL 'às' HH:mm'h'",
+        {
+            locale: ptBR,
+        }
+    );
 
-					<div className={styles.authorInfo}>
-						<strong>Víctor Barreto</strong>
-						<span>Web Developer</span>
-					</div>
-				</div>
+    const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+        locale: ptBR,
+        addSuffix: true,
+    });
 
-				<time title="11 de Maio às 08:13h" dateTime="2022-05-11 08:13:00">
-					Publicado há 1h
-				</time>
-			</header>
+    return (
+        <article className={styles.post}>
+            <header>
+                <div className={styles.author}>
+                    <Avatar src={author.avatarUrl} />
+                    <div className={styles.authorInfo}>
+                        <strong>{author.name}</strong>
+                        <span>{author.role}</span>
+                    </div>
+                </div>
 
-			<div className={styles.content}>
-				<p>Fala galeraa 👋</p>
-				<p>
-					Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
-					no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
-				</p>
-				<p>
-					<a href="">jane.design/doctorcare</a>
-				</p>
-				<p>
-					<a href="">#novoprojeto</a> <a href="">#nlw</a>{" "}
-					<a href="">#rocketseat</a>
-				</p>
-			</div>
+                <time
+                    title={publishedDateFormatted}
+                    dateTime={publishedAt.toISOString()}
+                >
+                    {publishedDateRelativeToNow}
+                </time>
+            </header>
 
-			<form className={styles.commentForm}>
-				<strong>Deixe seu feedback</strong>
+            <div className={styles.content}>
+                {content.map((line) => {
+                    if (line.type === "paragraph") {
+                        return <p>{line.content}</p>;
+                    } else if (line.type === "link") {
+                        return (
+                            <p>
+                                <a href="#">{line.content}</a>
+                            </p>
+                        );
+                    }
+                })}
+            </div>
 
-				<textarea placeholder="Deixe um comentário" />
+            <form className={styles.commentForm}>
+                <strong>Deixe seu feedback</strong>
 
-				<footer>
-					<button type="submit">Publicar</button>
-				</footer>
-			</form>
+                <textarea placeholder="Deixe um comentário" />
 
-			<div className={styles.commentList}>
-				<Comment />
-				<Comment />
-				<Comment />
-			</div>
-		</article>
-	);
+                <footer>
+                    <button type="submit">Publicar</button>
+                </footer>
+            </form>
+
+            <div className={styles.commentList}>
+                <Comment />
+                <Comment />
+                <Comment />
+            </div>
+        </article>
+    );
 }
