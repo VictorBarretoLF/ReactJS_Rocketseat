fed51e25bca93851c8b15693a2f16c15c2f41152
Víctor Barreto <victorbarretolins@gmail.com>
2023-04-09 16:06:02 -0300

    Cabeçalho da aplicação

--- 03-ignite-shop/src/pages/index.tsx
+++ 03-ignite-shop/src/pages/index.tsx
@@ -1,9 +1,24 @@
-import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "next/font/google";
+import { styled } from "@stitches/react";
 
-const inter = Inter({ subsets: ["latin"] });
+const Button = styled("button", {
+	backgroundColor: "$green300",
+	borderRadius: 4,
+	border: 0,
+	padding: "4px 8px",
+
+	span: {
+		fontWeight: "bold",
+	},
+
+	"&:hover": {
+		filter: "brightness(0.8)",
+	},
+});
 
 export default function Home() {
-	return <h1>Olá</h1>;
+	return (
+		<div>
+			<Button>Oi</Button>
+		</div>
+	);
 }
