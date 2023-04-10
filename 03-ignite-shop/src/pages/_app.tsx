ff4db4669e5d2e5f598f62eb96624328e1515863
Víctor Barreto <victor.barreto@behoh.com>
2023-04-10 14:42:29 -0300

    Estrutura da Home

--- 03-ignite-shop/src/pages/_app.tsx
+++ 03-ignite-shop/src/pages/_app.tsx
@@ -2,14 +2,16 @@ import type { AppProps } from "next/app";
 import { globalStyles } from "../styles/global";
 import logoImage from "../assets/logo.svg";
 import { Container, Header } from "@/styles/pages/app";
+import Image from "next/image";
 
 globalStyles();
+
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
 		<Container>
 			<Header>
-				<img src={logoImage.src} alt="" />
+				<Image src={logoImage} alt="" />
 			</Header>
 			<Component {...pageProps} />
 		</Container>
