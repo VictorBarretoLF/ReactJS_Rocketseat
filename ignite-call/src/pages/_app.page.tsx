5ab1eb5a4db83537d3672fdb1653be5dfcd28c0a
Víctor Barreto <victor.barreto@behoh.com>
2023-04-19 17:51:33 -0300

    Configurando o Next Auth

--- ignite-call/src/pages/_app.page.tsx
+++ ignite-call/src/pages/_app.page.tsx
@@ -1,10 +1,15 @@
 import type { AppProps } from "next/app";
 import { globalStyles } from "../styles/global";
+import { SessionProvider } from "next-auth/react";
 
 globalStyles();
 
-function App({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+    return (
+        <SessionProvider session={session}>
+            <Component {...pageProps} />
+        </SessionProvider>
+    );
 }
 
 export default App;
