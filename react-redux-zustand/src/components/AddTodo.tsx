a7cbcd2b9e787b13ed975798a88f2323fbdbe9c4
Víctor Barreto <victor.barreto@behoh.com>
2023-06-28 09:14:36 -0300

    Utilizando Redux DevTools

--- react-redux-zustand/src/components/AddTodo.tsx
+++ react-redux-zustand/src/components/AddTodo.tsx
@@ -1,23 +1,32 @@
 import { FormEvent, useState } from "react";
+import { useDispatch } from "react-redux";
+import { add } from "../store";
 
 export function AddTodo() {
-	const [newTodo, setNewTodo] = useState("");
+    const [newTodo, setNewTodo] = useState("");
+    const dispatch = useDispatch();
 
-	function handleNewTodo(e: FormEvent) {
-		e.preventDefault();
+    function handleNewTodo(e: FormEvent) {
+        e.preventDefault();
 
-		console.log(newTodo);
-	}
+        dispatch(
+            add({
+                newTodo,
+            })
+        );
 
-	return (
-		<form onSubmit={handleNewTodo}>
-			<input
-				type="text"
-				placeholder="Novo to-do"
-				value={newTodo}
-				onChange={(e) => setNewTodo(e.target.value)}
-			/>
-			<button type="submit">Adicionar</button>
-		</form>
-	);
+        setNewTodo("");
+    }
+
+    return (
+        <form onSubmit={handleNewTodo}>
+            <input
+                type="text"
+                placeholder="Novo to-do"
+                value={newTodo}
+                onChange={(e) => setNewTodo(e.target.value)}
+            />
+            <button type="submit">Adicionar</button>
+        </form>
+    );
 }
