12ba3368fcf6e6a56e4622732def3f5b4ab26850
Víctor Barreto <victorbarretolins@gmail.com>
2023-07-01 14:14:46 -0300

    configurando autoplay

--- react-redux-zustand/src/components/Module.tsx
+++ react-redux-zustand/src/components/Module.tsx
@@ -7,47 +7,57 @@ import { useDispatch } from "react-redux";
 import { play } from "../store/slices/player";
 
 interface ModuleProps {
-    moduleIndex: number;
-    title: string;
-    amountOfLessons: number;
+	moduleIndex: number;
+	title: string;
+	amountOfLessons: number;
 }
 
 export function Module({ moduleIndex, title, amountOfLessons }: ModuleProps) {
-    const dispatch = useDispatch();
+	const dispatch = useDispatch();
 
-    const lessons = useAppSelector((state) => {
-        return state.player.course.modules[moduleIndex].lessons;
-    });
+	const { currentModuleIndex, currentLessonIndex } = useAppSelector((state) => {
+		const { currentModuleIndex, currentLessonIndex } = state.player;
 
-    return (
-        <Collapsible.Root className="group">
-            <Collapsible.Trigger className="flex w-full items-center gap-3 bg-zinc-800 p-4">
-                <div className="flex h-10 w-10 rounded-full items-center justify-center bg-zinc-950 text-xs">
-                    {moduleIndex + 1}
-                </div>
+		return { currentModuleIndex, currentLessonIndex };
+	});
 
-                <div className="flex flex-col gap-1 text-left">
-                    <strong className="text-sm">{title}</strong>
-                    <span className="text-xs text-zinc-400">{amountOfLessons} aulas</span>
-                </div>
+	const lessons = useAppSelector((state) => {
+		return state.player.course.modules[moduleIndex].lessons;
+	});
 
-                <ChevronDown className="w-5 h-5 ml-auto text-zinc-400 group-data-[state=open]:rotate-180 transition-transform" />
-            </Collapsible.Trigger>
+	return (
+		<Collapsible.Root className="group" defaultOpen={moduleIndex === 0}>
+			<Collapsible.Trigger className="flex w-full items-center gap-3 bg-zinc-800 p-4">
+				<div className="flex h-10 w-10 rounded-full items-center justify-center bg-zinc-950 text-xs">
+					{moduleIndex + 1}
+				</div>
 
-            <Collapsible.Content>
-                <nav className="relative flex flex-col gap-4 p-6">
-                    {lessons.map((lesson, lessonIndex) => {
-                        return (
-                            <Lesson
-                                key={lesson.id}
-                                title={lesson.title}
-                                duration={lesson.duration}
-                                onPlay={() => dispatch(play([moduleIndex, lessonIndex]))}
-                            />
-                        );
-                    })}
-                </nav>
-            </Collapsible.Content>
-        </Collapsible.Root>
-    );
+				<div className="flex flex-col gap-1 text-left">
+					<strong className="text-sm">{title}</strong>
+					<span className="text-xs text-zinc-400">{amountOfLessons} aulas</span>
+				</div>
+
+				<ChevronDown className="w-5 h-5 ml-auto text-zinc-400 group-data-[state=open]:rotate-180 transition-transform" />
+			</Collapsible.Trigger>
+
+			<Collapsible.Content>
+				<nav className="relative flex flex-col gap-4 p-6">
+					{lessons.map((lesson, lessonIndex) => {
+						const isCurrent =
+							currentModuleIndex === moduleIndex && currentLessonIndex === lessonIndex;
+
+						return (
+							<Lesson
+								key={lesson.id}
+								title={lesson.title}
+								duration={lesson.duration}
+								isCurrent={isCurrent}
+								onPlay={() => dispatch(play([moduleIndex, lessonIndex]))}
+							/>
+						);
+					})}
+				</nav>
+			</Collapsible.Content>
+		</Collapsible.Root>
+	);
 }
--- react-redux-zustand/src/components/Video.tsx
+++ react-redux-zustand/src/components/Video.tsx
@@ -1,24 +1,34 @@
 import ReactPlayer from "react-player";
 import { useAppSelector } from "../store";
+import { useDispatch } from "react-redux";
+import { next } from "../store/slices/player";
 
 export function Video() {
-    const lesson = useAppSelector((state) => {
-        const { currentModuleIndex, currentLessonIndex } = state.player;
+	const dispatch = useDispatch();
 
-        const currentLesson =
-            state.player.course.modules[currentModuleIndex].lessons[currentLessonIndex];
+	const lesson = useAppSelector((state) => {
+		const { currentModuleIndex, currentLessonIndex } = state.player;
 
-        return currentLesson;
-    });
+		const currentLesson =
+			state.player.course.modules[currentModuleIndex].lessons[currentLessonIndex];
 
-    return (
-        <div className="w-full bg-zinc-950 aspect-video">
-            <ReactPlayer
-                width="100%"
-                height="100%"
-                controls
-                url={`https://www.youtube.com/watch?v=${lesson.id}`}
-            />
-        </div>
-    );
+		return currentLesson;
+	});
+
+	function handlePlayNext() {
+		dispatch(next());
+	}
+
+	return (
+		<div className="w-full bg-zinc-950 aspect-video">
+			<ReactPlayer
+				width="100%"
+				height="100%"
+				controls
+				playing
+				onEnded={handlePlayNext}
+				url={`https://www.youtube.com/watch?v=${lesson.id}`}
+			/>
+		</div>
+	);
 }
