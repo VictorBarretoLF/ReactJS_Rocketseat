0ba66eac4883f7b7550ba310cf98599d857c2fe2
Víctor Barreto <victor.barreto@behoh.com>
2023-07-04 17:08:17 -0300

    Carregando dados do curso

--- react-redux-zustand/src/components/Header.tsx
+++ react-redux-zustand/src/components/Header.tsx
@@ -1,13 +1,16 @@
-import { useAppSelector } from "../store";
 import { useCurrentLesson } from "../store/slices/player";
 
 export function Header() {
-	const { currentModule, currentLesson } = useCurrentLesson();
+    const { currentModule, currentLesson } = useCurrentLesson();
 
-	return (
-		<div className="flex flex-col gap-1">
-			<h1 className="text-2xl font-bold">{currentLesson.title}</h1>
-			<span className="text-sm text-zinc-400">Módulo "{currentModule.title}"</span>
-		</div>
-	);
+    if (!currentModule || !currentLesson) {
+        return null;
+    }
+
+    return (
+        <div className="flex flex-col gap-1">
+            <h1 className="text-2xl font-bold">{currentLesson.title}</h1>
+            <span className="text-sm text-zinc-400">Módulo "{currentModule.title}"</span>
+        </div>
+    );
 }
--- react-redux-zustand/src/components/Video.tsx
+++ react-redux-zustand/src/components/Video.tsx
@@ -1,27 +1,27 @@
 import ReactPlayer from "react-player";
-import { useAppSelector } from "../store";
 import { useDispatch } from "react-redux";
 import { next, useCurrentLesson } from "../store/slices/player";
 
 export function Video() {
-	const dispatch = useDispatch();
+    const dispatch = useDispatch();
+    const { currentLesson } = useCurrentLesson();
 
-	const { currentLesson } = useCurrentLesson();
+    function handlePlayNext() {
+        dispatch(next());
+    }
 
-	function handlePlayNext() {
-		dispatch(next());
-	}
+    if (!currentLesson) return null;
 
-	return (
-		<div className="w-full bg-zinc-950 aspect-video">
-			<ReactPlayer
-				width="100%"
-				height="100%"
-				controls
-				playing
-				onEnded={handlePlayNext}
-				url={`https://www.youtube.com/watch?v=${currentLesson.id}`}
-			/>
-		</div>
-	);
+    return (
+        <div className="w-full bg-zinc-950 aspect-video">
+            <ReactPlayer
+                width="100%"
+                height="100%"
+                controls
+                playing
+                onEnded={handlePlayNext}
+                url={`https://www.youtube.com/watch?v=${currentLesson.id}`}
+            />
+        </div>
+    );
 }
