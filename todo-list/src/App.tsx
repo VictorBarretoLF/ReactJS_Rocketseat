f5d33b608143ac9f96a68298b2e94a6b76062c12
Víctor Barreto <victorbarretolins@gmail.com>
2023-08-13 02:10:47 -0300

    feat: initialized the todo project

---
+++ todo-list/src/App.tsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import "./styles/global.css";
+
+function App() {
+	const [count, setCount] = useState(0);
+
+	return <div className="">oi</div>;
+}
+
+export default App;
