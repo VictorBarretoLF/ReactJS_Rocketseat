3ba5a577a7d699d90bfee8e8c3f95f5dd500d01f
˜Victor <victorbarretolins@gmail.com>
2023-09-03 13:24:56 -0300

    chore: reset do formulário

--- 02-ignite-timer/src/pages/Home/components/Countdown/index.tsx
+++ 02-ignite-timer/src/pages/Home/components/Countdown/index.tsx
@@ -1,40 +1,33 @@
 import { differenceInSeconds } from "date-fns";
-import { useEffect, useState } from "react";
+import { useContext, useEffect } from "react";
+import { CyclesContext } from "../../../../contexts/CyclesContext";
 import { CountdownContainer, Separator } from "./styles";
 
-interface CountdownProps {
-    activeCycle: any;
-    setCycles: any;
-    activeCycleId: any;
-}
-
-export function Countdown({ activeCycle, setCycles, activeCycleId }: CountdownProps) {
-    const [amountSecondsPassed, setAmountSecondsPassed] = useState(0);
+export function Countdown() {
+    const {
+        activeCycle,
+        activeCycleId,
+        markCurrentCycleAsFinished,
+        amountSecondsPassed,
+        setSecondsPassed,
+    } = useContext(CyclesContext);
 
     const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0;
 
     useEffect(() => {
-        let interval: ReturnType<typeof setInterval>;
+        let interval: number;
 
         if (activeCycle) {
             interval = setInterval(() => {
                 const secondsDifference = differenceInSeconds(new Date(), activeCycle.startDate);
 
                 if (secondsDifference >= totalSeconds) {
-                    setCycles((state) =>
-                        state.map((cycle) => {
-                            if (cycle.id === activeCycleId) {
-                                return { ...cycle, finishedDate: new Date() };
-                            } else {
-                                return cycle;
-                            }
-                        }),
-                    );
+                    markCurrentCycleAsFinished();
 
-                    setAmountSecondsPassed(totalSeconds);
+                    setSecondsPassed(totalSeconds);
                     clearInterval(interval);
                 } else {
-                    setAmountSecondsPassed(secondsDifference);
+                    setSecondsPassed(secondsDifference);
                 }
             }, 1000);
         }
@@ -42,7 +35,21 @@ export function Countdown({ activeCycle, setCycles, activeCycleId }: CountdownPr
         return () => {
             clearInterval(interval);
         };
-    }, [activeCycle, totalSeconds, activeCycleId]);
+    }, [activeCycle, totalSeconds, activeCycleId, setSecondsPassed, markCurrentCycleAsFinished]);
+
+    const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0;
+
+    const minutesAmount = Math.floor(currentSeconds / 60);
+    const secondsAmount = currentSeconds % 60;
+
+    const minutes = String(minutesAmount).padStart(2, "0");
+    const seconds = String(secondsAmount).padStart(2, "0");
+
+    useEffect(() => {
+        if (activeCycle) {
+            document.title = `${minutes}:${seconds}`;
+        }
+    }, [minutes, seconds, activeCycle]);
 
     return (
         <CountdownContainer>
